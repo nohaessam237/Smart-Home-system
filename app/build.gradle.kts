d7674b87efe6de79c83f30cd108c1b21b7658d1c
renadhaitham <renadhaitham.24@gmail.com>
2024-04-16 00:05:04 +0200

    Search ALL DONE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.smart_home_system"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.smart_home_system"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 30
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
